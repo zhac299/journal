e7462b7ef831a4a80479752701e216803269de5c
Muhammad Mirza <mirzm002@gmail.com>
2025-07-02 14:06:30 +0100

    created basic server.js file

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import records from './routes/records.js';
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/records', records);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
