eaf3a522e79d291dfb6b8dca277255ef2e37e100
Muhammad Mirza <mirzm002@gmail.com>
2025-07-02 16:36:42 +0100

    Setup Basic App - No Features

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
+import * as React from 'react'
 import { createRoot } from 'react-dom/client'
+import * as ReactDOM from "react-dom/client"
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+
+import RoutineItem from './components/RoutineItem'
+import RoutineItemList from './components/RoutineItemList'
+
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <RoutineItemList />,
+      },
+    ],
+  },
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
