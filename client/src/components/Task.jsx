80713aff12cdbc239a8b303100139bae3e6e92da
Muhammad Mirza <mirzm002@gmail.com>
2025-07-07 16:58:15 +0100

    Cleaned file and added code to generate all task cards

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -2,89 +2,57 @@ import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 export default function Task() {
-  const [form, setForm] = useState({
-    name: "",
-  done: false,
-  type: "",
-  routine: {
-    monday: false,
-    tuesday: false,
-    wednesday: false,
-    thursday: false,
-    friday: false,
-    saturday: false,
-    sunday: false
-  },
-  inWIP: false
-  });
-  const [isNew, setIsNew] = useState(true);
-  const params = useParams();
-  const navigate = useNavigate();
+  const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
-      const id = params.id?.toString() || undefined;
-      if(!id) return;
-      setIsNew(false);
-      const response = await fetch(
-        `http://localhost:5050/tasks`
-      );
+    // Fetch task data from the server
+    async function getTasks() {
+      const response = await fetch("http://localhost:5050/tasks");
       if (!response.ok) {
-        const message = `An error has occurred: ${response.statusText}`;
+        const message = `An error occurred: ${response.statusText}`;
         console.error(message);
         return;
       }
-      const task = await response.json();
-      if (!record) {
-        console.warn(`Record with id ${id} not found`);
-        navigate("/");
-        return;
-      }
-      setForm(task);
+      let tasks = await response.json();
+      setTasks(tasks);
+      console.log(tasks);
     }
-    fetchData();
+    getTasks();
     return;
-  }, [params.id, navigate]);
+  }, [tasks.length]);
 
-  // These methods will update the state properties.
-  function updateForm(value) {
-    return setForm((prev) => {
-      return { ...prev, ...value };
-    });
-  }
-
-  /**
-  // This function will handle the submission.
-  async function onSubmit(e) {
-    e.preventDefault();
-    const person = { ...form };
-    try {
-      let response;
-      if (isNew) {
-        // if we are adding a new record we will POST to /record.
-        response = await fetch("http://localhost:5050/record", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(person),
-        });
-      }
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-    } catch (error) {
-      console.error('A problem occurred with your fetch operation: ', error);
-    } finally {
-      setForm({ name: "", position: "", level: "" });
-      navigate("/");
-    }
-    **/
-
-  // This following section will display the form that takes the input from the user.
   return (
     <>
+      <div class="flex flex-col gap-4">
+        {tasks.map((task) => (
+          task.type === "other" ? (
+            <div className="flex justify-around basis-2/3 h-10 w-96 rounded-sm border border-black-600 shadow-md">
+              <div className="text-left indent-8 align-middle">
+                {task.name}
+              </div>
+
+              <div class="self-center">
+                <input type="checkbox" />
+              </div>
+
+              <div class="basis-6 self-end">
+                <img src="/remove.png" alt="Remove Sign"></img>
+              </div>
+            </div>
+          ) : (
+            <div className="flex justify-around basis-2/3 h-10 w-150 rounded-sm border border-black-600 shadow-md">
+              <div className="text-left indent-8 align-middle">
+                {task.name}
+              </div>
+
+              <div class="basis-6 self-end">
+                <img src="/remove.png" alt="Remove Sign"></img>
+              </div>
+            </div>
+          )
+
+        ))}
+      </div>
     </>
   );
 }
