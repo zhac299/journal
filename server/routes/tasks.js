2b124c27b9457e665529e38016a481353a59fe7c
Muhammad Mirza <mirzm002@gmail.com>
2025-07-07 16:56:45 +0100

    added a get call to get all other type tasks

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -11,10 +11,19 @@ import { ObjectId } from "mongodb";
 // The router will be added as a middleware and will take control of requests starting with path /tasks
 const router = express.Router();
 
-// This section will help you get a list of all routine items -- GET
+// This section will help you get a list of all tasks -- GET
 router.get("/", async (req, res) => {
     let collection = await db.collection("tasks");
     let results = await collection.find({}).toArray();
+    res.send(results).status(200);
+});
+
+// This section will help you get a list of all 'other' type of tasks-- GET
+router.get("/other", async (req, res) => {
+    let collection = await db.collection("tasks");
+    let results = await collection.find({}).toArray();
+    // Filter results to only include tasks of type 'other'
+    results = results.filter(task => task.type === "other");
     res.send(results).status(200);
 });
 
