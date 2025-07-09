fb7088e059e91887b76018017fa73023e979f106
Muhammad Mirza <mirzm002@gmail.com>
2025-07-09 21:54:36 +0100

    added endpoint to get all routine items

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -27,4 +27,13 @@ router.get("/other", async (req, res) => {
     res.send(results).status(200);
 });
 
+// This section will help you get a list of all 'routine' type of tasks -- GET
+router.get("/routine", async (req, res) => {
+    let collection = await db.collection("tasks");
+    let results = await collection.find({}).toArray();
+    // Filter results to only include tasks of type 'routine'
+    results = results.filter(task => task.type === "routine");
+    res.send(results).status(200);
+});
+
 export default router;
