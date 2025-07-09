6dd8a9d3e110204c11765eed97a259d70cc5f783
Muhammad Mirza <mirzm002@gmail.com>
2025-07-09 13:41:54 +0100

    deploy items added for application

--- server/server.js
+++ server/server.js
@@ -2,12 +2,28 @@ import express from 'express';
 import cors from 'cors';
 import tasks from './routes/tasks.js';
 
+import path from "path";
+
 const PORT = process.env.PORT || 5050;
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 app.use('/tasks', tasks);
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve static files from the React app
+    
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+  // Handle any requests that don't match the above routes with the React app
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // start the Express server
 app.listen(PORT, () => {
