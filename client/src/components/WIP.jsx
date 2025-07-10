cb827749d8b6a4aea063d9c38b54a1b56c1e8a64
Muhammad Mirza <mirzm002@gmail.com>
2025-07-10 16:17:46 +0100

    created a WIP task component.

---
+++ client/src/components/WIP.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+export default function WIP() {
+  const endpoint = "http://localhost:5050/tasks/"
+
+  var [otherWIPTasks, setOtherWIPTasks] = useState([]);
+
+  useEffect(() => {
+    async function getAllOtherWIPTasks() {
+      const response = await fetch(endpoint + "other/wip");
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        console.error(message);
+        return;
+      }
+      let other = await response.json();
+      setOtherWIPTasks(other);
+      console.log(other);
+    }
+    getAllOtherWIPTasks();
+    return;
+  }, [otherWIPTasks.length]);
+
+  return (
+    <>
+      <p class="text-left">WIP:</p>
+      <div class="flex flex-col gap-4">
+        {otherWIPTasks.map((otherWIPTask) => (
+          <div class="flex justify-around basis-2/3 h-5 w-150 rounded-sm border border-gray-400 shadow-md">
+            <div class="text-left align-middle items-start min-w-30 max-w-30 place-self-center">
+              {otherWIPTask.name}
+            </div>
+            <div>
+              <div class="columns-1 self-end">
+                <p>Done?</p>
+                <input type="checkbox" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
