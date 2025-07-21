fe9461d8d5f820a4124cc9f07fd0f087e9b3734e
Muhammad Mirza <mirzm002@gmail.com>
2025-07-21 18:49:41 +0100

    whitespace

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -34,7 +34,6 @@ export const Task = ({ _id, name, type }) => {
         </div>
       ) : (
         <div ref={setNodeRef} {...attributes} {...listeners} style={style} className="flex justify-around basis-2/3 h-5 w-125 rounded-sm border border-gray-400 shadow-md">
-
           {type === "wip" ? (
             <>
               <div className="text-left min-w-30 max-w-30 place-self-center text-nowrap">
