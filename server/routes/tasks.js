cc66b73e6268185f4fa7f46b4dbfcc2da0f89019
Muhammad Mirza <mirzm002@gmail.com>
2025-08-04 17:48:03 +0100

    added calls to get task by id and call to change positional value of task

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -13,36 +13,60 @@ const router = express.Router();
 
 // This section will help you get a list of all tasks -- GET
 router.get("/", async (req, res) => {
-    let collection = await db.collection("tasks");
-    let results = await collection.find({}).toArray();
-    res.send(results).status(200);
+  let collection = await db.collection("tasks");
+  let results = await collection.find({}).toArray();
+  res.send(results).status(200);
 });
 
 // This section will help you get a list of all 'other' type of tasks which are not in WIP-- GET
 router.get("/other/nwip", async (req, res) => {
-    let collection = await db.collection("tasks");
-    let results = await collection.find({}).toArray();
-    // Filter results to only include tasks of type 'other' and inWIP == false
-    results = results.filter(task => task.type === "other");
-    res.send(results).status(200);
+  let collection = await db.collection("tasks");
+  let results = await collection.find({}).toArray();
+  // Filter results to only include tasks of type 'other' and inWIP == false
+  results = results.filter((task) => task.type === "other");
+  res.send(results).status(200);
 });
 
 // This section will help you get a list of all wip tasks which are not routine -- GET
 router.get("/other/wip", async (req, res) => {
-    let collection = await db.collection("tasks");
-    let results = await collection.find({}).toArray();
-    // Filter results to only include tasks of type 'wip'
-    results = results.filter(task => task.type === "wip");
-    res.send(results).status(200);
+  let collection = await db.collection("tasks");
+  let results = await collection.find({}).toArray();
+  // Filter results to only include tasks of type 'wip'
+  results = results.filter((task) => task.type === "wip");
+  res.send(results).status(200);
 });
 
 // This section will help you get a list of all 'routine' type of tasks -- GET
 router.get("/routine", async (req, res) => {
-    let collection = await db.collection("tasks");
-    let results = await collection.find({}).toArray();
-    // Filter results to only include tasks of type 'routine'
-    results = results.filter(task => task.type === "routine");
-    res.send(results).status(200);
+  let collection = await db.collection("tasks");
+  let results = await collection.find({}).toArray();
+  // Filter results to only include tasks of type 'routine'
+  results = results.filter((task) => task.type === "routine");
+  res.send(results).status(200);
+});
+
+// This section will help you get a specific task based on id -- GET
+router.get("/:id", async (req, res) => {
+  let collection = await db.collection("tasks");
+  let results = await collection.find({}).toArray();
+  const id = req.params.id;
+  // Filter results to only include task with specified id
+  results = results.filter((task) => task._id === id);
+  res.send(results).status(200);
+});
+
+router.post("/:id/order/:pos", async (req, res) => {
+  let collection = await db.collection("tasks");
+  let results = await collection.find({}).toArray();
+  const id = req.params.id;
+  const pos = req.params.pos;
+  // Filter results to only include task with specified id
+  results = results.filter((task) => task._id === id);
+  results.forEach((task) => {
+    task.position = pos;
+  });
+  console.log(results);
+  res.send(results).status(200);
 });
 
 export default router;
